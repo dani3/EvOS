698da1226cac905d679fb372b6533167fcde82bd
Dani M <daniel.m.aldea@hotmail.com>
2021-07-07 21:51:51 +0200

    added makefile and routine to load a message from a file

--- archive/interrupts/boot.asm
+++ archive/interrupts/boot.asm
@@ -1,54 +1,62 @@
 ORG 0
 BITS 16
 
+; https://wiki.osdev.org/FAT
+; Implement a fake BPB.
 _start:
+    ; The first three bytes are expected from the BIOS.
+    ; A short jump
     jmp short start
+    ; And a nop.
     nop
 
+; Fill the rest of the BPB (33 bytes).
 times 33 db 0
 
 start:
     ; Jump to our code, some BIOSs might not do this themselves.
+    ; Replace the code segment register with 0x7c0.
     jmp 0x7c0:step2
+
+handle_zero:
+    mov ah, 0x0e
+    mov al, 'A'
+    int 0x10
+    iret
 
 step2:
     ; Clear interrupts while we change the data segment
     cli
 
     mov ax, 0x7c0
+    ; Set data segment
     mov ds, ax
+    ; Set extra segment
     mov es, ax
+
     mov ax, 0x00
+    ; Set Stack segment
     mov ss, ax
+    ; Set stack pointer
     mov sp, 0x7c00
 
     ; Enable interrupts
     sti
 
-    mov ah, 0x02    ; Read sector command
-    mov al, 1       ; One sector to read
-    mov ch, 0       ; Cylunder low eight bits
-    mov cl, 2       ; Read sector two
-    mov dh, 0       ; Head number
-    mov bx, buffer
-    int 0x13
-    ; Jump carry
-    jc error
+    ; Offset
+    mov word[ss:0x00], handle_zero
+    ; Segment
+    mov word[ss:0x02], 0x7c0
 
-    mov si, buffer
+    int 0
+
+    ; Move the address of the label to si
+    mov si, message
     call print
-
-    ; Infinite loop
-    jmp $
-
-error:
-    mov si, error_message
-    call print
-
-    ; Infinite loop
     jmp $
 
 print:
+    ; Page number
     mov bx, 0
 .loop:
     ; Load the char of the "si" register and load it into the "al" register.
@@ -70,12 +78,9 @@ print_char:
     int 0x10
     ret
 
-error_message: db 'Failed to load sector', 0
+message: db 'Hello Wordl!', 0
 
 ; Pad the rest with zeroes.
 times 510-($ - $$) db 0
 ; Add the bootloader signature (little-endian).
 dw 0xAA55
-
-; Placeholder for placing our message outside the boot sector.
-buffer:
