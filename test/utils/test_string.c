3886bec8763918da73a1001ebc766ad29f0efe35
Dani M <daniel.m.aldea@hotmail.com>
2021-07-19 21:29:52 +0200

    added unit tests for string module using ceedling

--- test/utils/test_string.c
+++ test/utils/test_string.c
@@ -1,8 +1,8 @@
 #ifdef TEST
 
-#  include "unity.h"
+#include "unity.h"
 
-#  include "string.h"
+#include <utils/string.h>
 
 void setUp(void) {
 }
@@ -10,20 +10,14 @@ void setUp(void) {
 void tearDown(void) {
 }
 
-void test_string_emptyString(void) {
+void test_string_notEmpty(void) {
+  char* message = "This is a message";
+  TEST_ASSERT_EQUAL_INT(strlen(message), 17);
+}
+
+void test_string_empty(void) {
   char* message = "";
   TEST_ASSERT_EQUAL_INT(strlen(message), 0);
-}
-
-void test_string_oneElement(void) {
-  char* message = "a";
-  TEST_ASSERT_EQUAL_INT(strlen(message), 1);
-}
-
-void test_string_largeString(void) {
-  char* message =
-      "This is a relative large message. The strlen function should be able to handle this quickly";
-  TEST_ASSERT_EQUAL_INT(strlen(message), 91);
 }
 
 #endif // TEST
